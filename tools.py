e0f27130202256a49c348bbe8ea136474ae5c41a
grilo <joao.grilo@gmail.com>
2016-11-06 06:43:45 +0100

    Make the alternatives command also work in RHEL.

--- tools.py
+++ tools.py
@@ -10,19 +10,19 @@ import plistlib
 
 
 def get_version(file):
-	version = None
-	if file.endswith('.apk'):
-		version = read_apk_version(file)
-	elif file.endswith('.jar') or file.endswith('.war') or file.endswith('.ear'):
-		return read_jar_version(file)
-	elif file.endswith('.ipa'):
-		return read_ipa_version(file)
-	return version
+    version = None
+    if file.endswith('.apk'):
+        version = read_apk_version(file)
+    elif file.endswith('.jar') or file.endswith('.war') or file.endswith('.ear'):
+        return read_jar_version(file)
+    elif file.endswith('.ipa'):
+        return read_ipa_version(file)
+    return version
 
 def read_apk_version(file):
-	cmd = 'AAPT_TOOL="$ANDROID_HOME/build-tools/$(ls -rt $ANDROID_HOME/build-tools | tail -1)/aapt"\n'
-	cmd += 'VERSION=$($AAPT_TOOL d badging "%s" | grep versionName | awk -F\\\' \'{print $4"-"$6}\')' % (file)
-	return cmd
+    cmd = 'AAPT_TOOL="$ANDROID_HOME/build-tools/$(ls -rt $ANDROID_HOME/build-tools | tail -1)/aapt"\n'
+    cmd += 'VERSION=$($AAPT_TOOL d badging "%s" | grep versionName | awk -F\\\' \'{print $4"-"$6}\')' % (file)
+    return cmd
 
 def read_jar_version(file):
     return "unzip -p '%s' \*/pom.properties | grep version | awk -F= '{print $2}'" % (file)
@@ -31,14 +31,17 @@ def read_ipa_version(file):
     return "VERSION=$(/usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' /dev/stdin <<< $(unzip -p '%s' \*/Info.plist))" % (file)
 
 def get_alternatives(binary):
-	rc, out = run_cmd('update-alternatives --list %s' % (binary))
-	return out.splitlines()
+    try:
+        rc, out = self.run_cmd('update-alternatives --list %s' % (binary))
+    except OSError:
+        rc, out = self.run_cmd('/usr/sbin/update-alternatives --display %s' % (binary))
+    return out.splitlines()
 
 def run_cmd(cmd):
-	p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-	stdout, stderr = p.communicate()
-	return p.returncode, stdout
+    p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    stdout, stderr = p.communicate()
+    return p.returncode, stdout
 
 def get_executable(executable):
-	# Not sure if this is python3 compatible
-	return distutils.spawn.find_executable(executable)
+    # Not sure if this is python3 compatible
+    return distutils.spawn.find_executable(executable)
