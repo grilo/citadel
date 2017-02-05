ddabb07e9a9f571b196694d83a98a6a8ddfddc1b
grilo <joao.grilo@gmail.com>
2017-02-05 06:33:08 +0100

    Remove the github pages, causing readthedocs to break.

--- docs/conf.py
+++ docs/conf.py
@@ -34,8 +34,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.viewcode',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
