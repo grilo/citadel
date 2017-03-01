e14d840bf333f09899804df0f94bd6e5262e739b
Joao Grilo <joao.grilo@gmail.com>
2017-03-01 23:51:45 +0100

    Merge changes from downstream.

--- citadel/nodes/npmrc.py
+++ citadel/nodes/npmrc.py
@@ -76,37 +76,26 @@ class Npmrc(citadel.nodes.node.Base):
 
     def __init__(self, yml, path):
         super(Npmrc, self).__init__(yml, path)
-
-        self.parser.add_default('always_auth', 'false')
-        self.parser.add_default('strict_ssl', 'false')
+        self.parser.add_default('always-auth', 'false')
+        self.parser.add_default('strict-ssl', 'false')
         self.parser.add_default('token', 'undefined')
-
-        self.parser.is_required('registry')
-        self.parser.is_required('email')
-
         errors, parsed, ignored = self.parser.validate()
         self.errors.extend(errors)
-
         self.output.append(
             self.generate_npmrc(
-                parsed['always_auth'],
-                parsed['strict_ssl'],
+                parsed['always-auth'],
+                parsed['strict-ssl'],
                 parsed['token'],
-                parsed['registry'],
-                parsed['email'],
             )
         )
-
         for k, v in ignored.items():
             self.output.append('echo "%s: %s" >> .npmrc' % (k, v))
 
-
-    def generate_npmrc(self, always_auth, strict_ssl, token, registry, email):
-        return """echo "Generating .npmrc file. If we're having authentication
-problems, ensure the variable $NPMAUTH is being passed through citadel."
-echo "email: %s" > .npmrc
+    def generate_npmrc(self, always_auth, strict_ssl, token):
+        return """
+echo "Generating .npmrc file. If we're having authentication problems, ensure
+the variable \$NPMAUTH is being passed through citadel."
 echo "always-auth: %s" >> .npmrc
 echo "strict-ssl: %s" >> .npmrc
-echo "registry: %s" >> .npmrc
 echo "_token: %s" >> .npmrc
-""" % (always_auth, strict_ssl, token, registry, email)
+""" % (always_auth, strict_ssl, token)
